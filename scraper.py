870a3f1425724a7987ad9a1f4e2a93e91dc89103
Siggerud <102146900+Siggerud@users.noreply.github.com>
2023-02-08 20:40:34 +0100

    files modified

--- scraper.py
+++ scraper.py
@@ -1,17 +1,104 @@
 import requests
 from bs4 import BeautifulSoup
 
-response = requests.get("https://www.yr.no/nb/v%C3%A6rvarsel/timetabell/1-305409/Norge/Troms%20og%20Finnmark/Troms%C3%B8/Troms%C3%B8?i=0")
-response.raise_for_status()
+class WeatherScraper:
+    def __init__(self, baseUrl, day, fromHour, toHour):
+        self._baseUrl = baseUrl
+        self._day = day
+        self._fromHour = fromHour
+        self._toHour = toHour
+        self._rowTags = self._getTags()
 
+
+    def _getTags(self):
+        response = requests.get(self._baseUrl + f"?i={self._day}")
+        response.raise_for_status()
+
+        soup = BeautifulSoup(response.content, "html.parser")
+        tags = soup.find_all("tr", class_="fluid-table__row")
+
+        return tags
+
+    def getWeatherDescriptions(self):
+        weatherDesc = []
+        for tag in self._rowTags:
+            hour = tag.find("time").text
+            if self._fromHour <= int(hour) < self._toHour:
+                image = tag.find("img", alt=True)
+                description = image["alt"]
+                weatherDesc.append(description)
+
+            if int(hour) >= self._toHour:
+                break
+
+        return weatherDesc
+
+    def getTemperatures(self):
+        temperatures = []
+        for tag in self._rowTags:
+            hour = tag.find("time").text
+            if self._fromHour <= int(hour) < self._toHour:
+                temp = tag.find("span", class_="temperature__degree").parent.text[:-1]
+                temperatures.append(int(temp))
+
+            if int(hour) >= self._toHour:
+                break
+
+        return temperatures
+
+    def getWinds(self):
+        winds = []
+
+        for tag in self._rowTags:
+            hour = tag.find("time").text
+            if self._fromHour <= int(hour) < self._toHour:
+                wind = tag.find("span", class_="wind__value").text
+                winds.append(int(wind))
+
+            if int(hour) >= self._toHour:
+                break
+        return winds
+
+    def getWindGusts(self):
+        windGusts = []
+
+        for tag in self._rowTags:
+            hour = tag.find("time").text
+            if self._fromHour <= int(hour) < self._toHour:
+                gust = tag.find("span", class_="wind__gust").text[2:-1]
+                windGusts.append(int(gust))
+
+            if int(hour) >= self._toHour:
+                break
+        return windGusts
+
+    def getMinAndMaxPrecipitation(self):
+        precipitationRange = [0, 0]
+
+        for tag in self._rowTags:
+            hour = tag.find("time").text
+            if self._fromHour <= int(hour) < self._toHour:
+                precipitations = tag.find_all("span", class_="Precipitation-module__main-sU6qN")
+                if len(precipitations) != 2:
+                    continue
+                for index, precip in enumerate(precipitations):
+                    precipitationRange[index] += float(precip.text.replace(",", "."))
+
+            if int(hour) >= self._toHour:
+                break
+
+        return precipitationRange
+
+
+
+
+"""
 weatherDesc = []
 temperatures = []
 winds = []
 windgusts = []
 precipitationRange = [0, 0]
 
-
-soup = BeautifulSoup(response.content, "html.parser")
 tags = soup.find_all("tr", class_="fluid-table__row")
 for tag in tags[:5]:
     image = tag.find("img", alt=True)
@@ -54,3 +141,4 @@ for description in weatherDesc:
 
 print(okayToSki)
 print(precipitationRange)
+"""
--- skiWeatherGUI.py
+++ skiWeatherGUI.py
@@ -1,28 +1,177 @@
-from tkinter import Tk, Label, ttk, StringVar
+from tkinter import Tk, Label, ttk, StringVar, Button
 from datetime import datetime, timedelta
+from scraper import WeatherScraper
+from skiLubrication import SkiLubrication
 
 class SkiWeatherGUI:
     def __init__(self, master):
         self._master = master
         self._master.title("Ski weather checker")
-        self._master.geometry("350x500")
+        self._master.geometry("400x400")
+
+        self._temperatures = []
+
+        whereLabel = Label(master, text="Where do you want to ski?")
+        whereLabel.grid(row=0, column=0, sticky="w", columnspan=2, pady=1)
+
+        self._firstPartUrl = "https://www.yr.no/nb/v%C3%A6rvarsel/timetabell/"
+        self._placesUrls = {"Badedammen": "1-2226455/Norge/Oslo/Oslo/Badedammen",
+                            "Nøklevann": "1-74028/Norge/Oslo/Oslo/N%C3%B8klevannet",
+                            "Finnemarka": "1-72002/Norge/Viken/Modum/Finnemarka",
+                            "Sollihøgda": "1-72655/Norge/Viken/Bærum/Sollihøgda"}
+
+        self._places = StringVar()
+        self._placesChosen = ttk.Combobox(master, width=15, textvariable=self._places)
+        self._placesChosen["values"] = list(self._placesUrls.keys())
+        self._placesChosen.grid(row=1, column=0, pady=5, sticky="w", columnspan=2)
+        self._placesChosen.current(0)
+
+        timeLabel = Label(master, text="When do you want to ski?")
+        timeLabel.grid(row=2, column=0, sticky="w", columnspan=2, pady=1)
+
+        dateLabel = Label(master, text="Date")
+        dateLabel.grid(row=3, column=0)
+
+        fromHourLabel = Label(master, text="From")
+        fromHourLabel.grid(row=3, column=1)
+
+        toHourLabel = Label(master, text="To")
+        toHourLabel.grid(row=3, column=2)
 
         self._day = StringVar()
-        dayChosen = ttk.Combobox(master, width=10, textvariable= self._day)
-        dayChosen["values"] = self._getDates()
-        dayChosen.grid(row=1, column=1)
-        dayChosen.current()
+        self._dayChosen = ttk.Combobox(master, width=10, textvariable= self._day)
+        self._dayChosen["values"] = self._getDates()
+        self._dayChosen.grid(row=4, column=0, padx=10, pady=5, sticky="w")
+        self._dayChosen.current(0)
 
         self._hour = StringVar()
         self._hourChosen = ttk.Combobox(master, width=10, textvariable= self._hour)
+        self._hourChosen.grid(row=4, column=1, padx=10, sticky="w")
+
+        self._dayChosen.bind("<<ComboboxSelected>>", self._setHours)
+
+        self._toHour = StringVar()
+        self._toHourChosen = ttk.Combobox(master, width=10, textvariable=self._toHour)
         self._getHours()
-        self._hourChosen.grid(row=1, column=2)
-        self._hourChosen.current()
+        self._toHourChosen.grid(row=4, column=2)
+        self._hourChosen.current(0)
+        self._toHourChosen.current(0)
 
-        dayChosen.bind("<<ComboboxSelected>>", self._setHours)
+        self._hourChosen.bind("<<ComboboxSelected>>", self._setToHours)
 
-    def setToHours(self, event):
+        execute = Button(master, bg="cyan", text="Check", command=self._addWeatherAndLubricationInfo)
+        execute.grid(row=5, column=1)
+
+        self._descLabel = Label(master)
+        self._descLabel.grid(row=6, column=0, columnspan=4, sticky="w")
+
+        self._temperatureLabel = Label(master)
+        self._temperatureLabel.grid(row=7, column=0, columnspan=4, sticky="w")
+
+        self._windLabel = Label(master)
+        self._windLabel.grid(row=8, column=0, columnspan=4, sticky="w")
+
+        self._precipitationLabel = Label(master)
+        self._precipitationLabel.grid(row=9, column=0, columnspan=4, sticky="w")
+
+        self._lubricationLabel = Label(master)
+        self._lubricationLabel.grid(row=10, column=0, columnspan=4, sticky="w")
+
+    def _addWeatherAndLubricationInfo(self):
+        self._addWeatherInfo()
+        self._addLubricationInfo()
+    def _addWeatherInfo(self):
+        day = self._dayChosen.current()
+        fromHour = int(self._hour.get())
+        toHour = int(self._toHour.get())
+        url = self._firstPartUrl + self._placesUrls[self._places.get()]
+        weatherInfo = WeatherScraper(url, day, fromHour, toHour)
+        self._setWeatherDescription(weatherInfo.getWeatherDescriptions())
+        self._temperatures = weatherInfo.getTemperatures()
+        self._setTemperatureLabel(self._temperatures)
+        self._setWindLabel(weatherInfo.getWinds(), weatherInfo.getWindGusts())
+        self._setPrecipitationLabel(weatherInfo.getMinAndMaxPrecipitation())
+
+    def _addLubricationInfo(self):
+        lubricator = SkiLubrication(self._temperatures)
+        lubricants = lubricator.getLubricationTip()
+
+        if len(lubricants) == 0:
+            text = f"No good lube suggestions in these temperatures"
+        elif len(lubricants) == 1:
+            text = f"For grip you should use {lubricants[0]} lube"
+        elif len(lubricants) == 2:
+            text = f"For grip you should mainly use {lubricants[0]} lube, but also bring {lubricants[1]} lube"
+        elif len(lubricants) > 2:
+            text = f"For grip you should mainly use {lubricants[0]} lube, but also bring {lubricants[1]}"
+            for i in range(2, len(lubricants)):
+                if i == len(lubricants) - 1:
+                    text += f"and {lubricants[i]} lube"
+                else:
+                    text += f",{lubricants[i]} "
+
+        self._lubricationLabel.config(text=text)
+
+    def _setPrecipitationLabel(self, minAndMaxPrecipitation):
+        if minAndMaxPrecipitation[1] != 0:
+            text = f"Expect precipitation between {minAndMaxPrecipitation[0]}mm and {minAndMaxPrecipitation[1]}mm"
+        else:
+            text = "No precipitation expected"
+
+        self._precipitationLabel.config(text=text)
+    def _setWindLabel(self, winds, gusts):
+        maxWind = max(winds)
+        maxGust = max(gusts)
+
+        text = f"Max wind will be {maxWind} m/s with gusts up to {maxGust} m/s"
+
+        self._windLabel.config(text=text)
+
+    def _setTemperatureLabel(self, temperatures):
+        minTemp = min(temperatures)
+        maxTemp = max(temperatures)
+
+        numOfUniqueValues = len(set(temperatures))
+
+        if numOfUniqueValues == 1:
+            text = f"The temperature will stay at {maxTemp}°C"
+        else:
+            text = f"The temperature will range from {minTemp}°C to {maxTemp}°C "
+
+        self._temperatureLabel.config(text=text)
+
+    def _setWeatherDescription(self, weatherDescriptions):
+        weatherDictionary = {'tåke': 'foggy', 'skyet': 'cloudy', 'delvis skyet': 'partly cloudy',
+                             'lettskyet': 'some clouds', 'regn': 'rain', 'klarvær': 'no clouds'}
+
+        uniqueDescriptions = []
+        for description in weatherDescriptions:
+            if description not in uniqueDescriptions:
+                uniqueDescriptions.append(description)
+
+        if len(uniqueDescriptions) == 1:
+            text = f"You can expect it to be {weatherDictionary[uniqueDescriptions[0]]} in the timeframe"
+        else:
+            text = f"The weather will go from {weatherDictionary[uniqueDescriptions[0]]}"
+            for i in range(1, len(uniqueDescriptions)):
+                text += f" to {uniqueDescriptions[i]}"
+        self._descLabel.config(text=text)
+
+
+
+    def _setToHours(self, event):
         hours = []
+        fromHour = int(self._hour.get())
+
+        for i in range(fromHour+1, 24):
+            if i < 10:
+                hour = "0" + str(i)
+            else:
+                hour = str(i)
+            hours.append(hour)
+
+        self._toHourChosen["values"] = hours
+        self._toHourChosen.current(0)
 
     def _getDates(self):
         today = datetime.now().strftime("%a %d.%m")
@@ -49,6 +198,7 @@ class SkiWeatherGUI:
             hours.sort()
 
         self._hourChosen["values"] = hours
+        self._hourChosen.current(0)
 
     def _getHours(self):
         hours = []
@@ -65,6 +215,7 @@ class SkiWeatherGUI:
             hours.sort()
 
         self._hourChosen["values"] = hours
+        self._toHourChosen["values"] = hours
 
 
 
